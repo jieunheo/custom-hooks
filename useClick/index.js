83aefaa97c5ed6fed6cc043e47a745abd2d61ae7
heojieun <gjekdmlwl@naver.com>
2022-06-20 17:00:09 +0900

    useClick Hook 클릭 이벤트를 추가하는 Hook useRef()를 사용하여 element에 접근하기

---
+++ useClick/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { useClick } from "./useClick";
+
+const App = () => {
+  const sayHello = () => {
+    console.log("Hello");
+  };
+  const title = useClick(sayHello);
+
+  return (
+    <div className="App">
+      <h1 ref={title}>Hi</h1>
+    </div>
+  );
+};
+
+export default App;
