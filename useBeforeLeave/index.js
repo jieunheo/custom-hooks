45d8e2c6555861cb6fca6e8f262706449f512580
heojieun <gjekdmlwl@naver.com>
2022-06-21 10:37:51 +0900

    useBeforeLeave Hook 마우스가 document를 벗어날 때 알림을 실행하는 Hook 사라질 때 마우스의 Y값이 0인 경우에만 실행

---
+++ useBeforeLeave/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useBeforeLeave } from "./useBeforeLeave";
+
+const App = () => {
+  const begForLife = () => console.log("Pls don't leave");
+  useBeforeLeave(begForLife);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default App;
