6e0117552ee19babc68ceef323b34e2dcdef2494
heojieun <gjekdmlwl@naver.com>
2022-06-21 13:14:59 +0900

    useNetwork Hook return을 빼먹음

--- useNetwork/useNetwork.js
+++ useNetwork/useNetwork.js
@@ -13,7 +13,7 @@ export const useNetwork = (onChange) => {
     window.addEventListener("online", handleChange);
     window.addEventListener("offline", handleChange);
 
-    () => {
+    return () => {
       window.removeEventListener("online", handleChange);
       window.removeEventListener("offline", handleChange);
     };
